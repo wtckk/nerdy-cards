c1873669023ec1fe63428835f3d359fc1d2c30d1
dyakovmaksim <pochta3478@mail.ru>
2024-04-04 20:11:51 +0600

    Footer buttons
    добавил кнопошки в футере

--- frontend/components/Footer/index.vue
+++ frontend/components/Footer/index.vue
@@ -1,6 +1,10 @@
 <template>
-    <footer class="flex ">
-        1
+    <footer class="flex justify-content py-[40px] bg-[#432671] px-[10px] ">
+        <div class="flex align-items center  gap-3">
+            <button class="bg-[#CBAAFF] rounded-xl px-2 py-1">О компании</button>
+            <button class="bg-[#CBAAFF] rounded-xl px-5 py-1">Реклама</button>
+            <button class=" bg-[#CBAAFF] rounded-xl px-2 py-1">Поддержка</button>
+        </div>
     </footer>
 </template>
 
