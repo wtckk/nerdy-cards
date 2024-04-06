738ed59078749a1be37dcfd75d5987fa3afbca30
dyakovmaksim <pochta3478@mail.ru>
2024-04-06 12:57:07 +0600

    register
    добавлена форма регистрации

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>registration</div>
+  <div class="flex justify-center mt-32">
+    <form class="bg-[#432671] shadow-md rounded-3xl px-8 pt-6 pb-8 mb-4 w-96">
+      <p class="flex justify-center text-white text-2xl font-bold mb-4">
+        Регистрация
+      </p>
+      <div class="mb-4">
+        <label class="block text-white text-sm font-bold mb-2" for="username">
+          Логин
+        </label>
+        <input
+          class="bg-[#CBAAFF] shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Логин">
+        <p class="text-red-500 text-xs italic">Введите логин</p>
+      </div>
+      <div class="mb-6">
+        <label class="block text-white text-sm font-bold mb-2" for="password">
+          Почта
+        </label>
+        <input
+          class="bg-[#CBAAFF] shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="mail" type="mail" placeholder="Почтовый адрес">
+        <p class="text-red-500 text-xs italic">Введите почту</p>
+      </div>
+      <div class="mb-6">
+        <label class="block text-white text-sm font-bold mb-2" for="password">
+          Пароль
+        </label>
+        <input
+          class="bg-[#CBAAFF] shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password" type="password" placeholder="***********">
+        <p class="text-red-500 text-xs italic">Введите пароль</p>
+      </div>
+      <div class="mb-6">
+        <label class="block text-white text-sm font-bold mb-2" for="password">
+          Повторите пароль
+        </label>
+        <input
+          class="bg-[#CBAAFF] shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password" type="password" placeholder="***********">
+        <p class="text-red-500 text-xs italic">Введите пароль</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-[#CBAAFF] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button">
+          Зарегистрироваться
+        </button>
+      </div>
+      <p class="mb-0 mt-2 pt-3 text-sm text-gray-500">
+        Уже зарегистрированы?
+        <a href="#!"
+          class="text-[#CBAAFF] text-danger transition duration-150 ease-in-out hover:hover:text-purple-800 focus:text-danger-600 active:text-danger-700">Войти</a>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
