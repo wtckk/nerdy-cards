ad2768618b4910c7cd8172430a1cab1397292f36
shadowoem <bessonovads@yandex.ru>
2024-04-07 10:30:05 +0500

    .

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="flex flex-col h-full">
-    <div class="h-[25%]">
-      <p class="text-[20px] text-white">Новые</p>
-      <div class="overflow-x-auto flex gap-[45px]">
-        <div class="w-[50%]" v-for="module in Modules" :key="module.id">
+  <div class="flex flex-col h-full py-3">
+    <div class="custom-scrollbar h-[35%]">
+      <p class="text-[20px] text-white py-2">Новые</p>
+      <div class="flex gap-[45px]">
+        <div class="w-[50%] py-2" v-for="module in Modules" :key="module.id">
           <ModuleCard :module-card="module" />
         </div>
       </div>
     </div>
-    <div>
-      <p class="text-[20px] text-white">Мои</p>
+    <div class="h-[35%] custom-scrollbar">
+      <p class="text-[20px] text-white py-2">Мои</p>
+      <div class="flex gap-[45px]">
+        <div class="w-[50%] py-2" v-for="module in Modules" :key="module.id">
+          <ModuleCard :module-card="module" />
+        </div>
+      </div>
     </div>
-    <div>
-      <p class="text-[20px] text-white">Популярные</p>
+    <div class="h-[35%] custom-scrollbar">
+      <p class="text-[20px] text-white py-2">Популярные</p>
+      <div class="flex gap-[45px]">
+        <div class="w-[50%] py-2" v-for="module in Modules" :key="module.id">
+          <ModuleCard :module-card="module" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -237,7 +247,69 @@ const Modules = ref<Module[]>([
       },
     ],
   },
+  {
+    id: "465546543",
+    ownerId: "4343",
+    name: "Модуль 5",
+    likes: 1,
+    createAt: "05.06.2021",
+    cards: [
+      {
+        id: "5434534545",
+        modulId: "465546543",
+        term: "Junior",
+        position: 1,
+        definition: "новичок",
+        isLearned: false,
+      },
+      {
+        id: "85465656",
+        modulId: "465546543",
+        term: "Middle",
+        position: 2,
+        definition: "средний",
+        isLearned: false,
+      },
+      {
+        id: "877675435453",
+        modulId: "465546543",
+        term: "Senior",
+        position: 3,
+        definition: "старший",
+        isLearned: false,
+      },
+    ],
+    tags: [
+      {
+        id: "65544763",
+        creatorId: "4343",
+        name: "ИТ",
+      },
+    ],
+  },
 ]);
 </script>
 
-<style scoped></style>
+<style scoped>
+/* <template>
+  <div class="flex flex-col h-full custom-scrollbar">
+    <!-- Ваш текущий контент здесь -->
+  </div>
+</template> */
+
+.custom-scrollbar {
+  overflow-x: scroll;
+  scrollbar-width: thin; /* Ширина скроллбара (Windows, MacOS) */
+  scrollbar-color: rgb(167, 139, 250); /* Цвет скроллбара (для Chrome, Edge) */
+}
+
+.custom-scrollbar::-webkit-scrollbar {
+  width: 6px; /* Ширина скроллбара (для Webkit браузеров) */
+  height: 10px;
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb {
+  background-color: rgb(64, 43, 126); /* Цвет ползунка скроллбара */
+  border-radius: 20px; /* Радиус закругления ползунка */
+}
+</style>
