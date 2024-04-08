f3fb374e588b30dd53c0875a993985258b9eca8c
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2024-04-08 16:39:44 +0500

    1

--- frontend/components/ModuleCard/index.vue
+++ frontend/components/ModuleCard/index.vue
@@ -2,7 +2,6 @@
   <div
     class="flex gap-[30px] flex-col rounded-md bg-violet-400 p-3 text-white w-[320px]"
   >
-    <nuxt-icon name="🚀" />
     <div class="gap-[16px] flex flex-col">
       <div class="text-base">{{ props.moduleCard.name }}</div>
       <div class="rounded-full px-2 w-[30%] bg-violet-500 text-sm">
